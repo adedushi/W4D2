7e9524c65b44be183b7cc1daa695b82b9f96be79
Albert Dedushi <adedushi@hotmail.com>
2023-11-07 17:32:51 -0500

    Add #diganol_dirs

--- practice-for-ch-ruby-chess-long-practice-main/slideable.rb
+++ practice-for-ch-ruby-chess-long-practice-main/slideable.rb
@@ -1,3 +1,34 @@
-# module Slideable 
+module Slideable 
+
     def diagonal_dirs(pos)
-   
+        DIAGNOL_DIRS.each do |ele| 
+            x, y = pos
+            dx, dy = ele
+            new_move = []
+            new_move << [dx + x, dy + y]
+        end
+
+        new_move.select!{ |ele| ele.all?{ |coord| coord < 8 && coord >= 0 }}
+    end
+
+    def horizonal_dirs(pos)
+    end
+
+    def moves
+    end
+
+
+    private
+    HORIZONTAL_DIRS =
+    [[1,0],
+    [-1,0],
+    [0,1],
+    [0,-1]]
+
+    DIAGNOL_DIRS =
+    [[1,1],
+    [-1,-1],
+    [1,-1],
+    [-1,1]]
+
+end
