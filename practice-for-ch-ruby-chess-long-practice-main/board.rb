d676b60482fb6b51535094ee340a044e90351763
Shaun Jhingoor <Jhingoor1945@gmail.com>
2023-11-07 16:27:49 -0500

    Might of figure out move_piece

--- practice-for-ch-ruby-chess-long-practice-main/board.rb
+++ practice-for-ch-ruby-chess-long-practice-main/board.rb
@@ -1,7 +1,12 @@
+require_relative 'Piece'
 class Board 
     attr_reader :rows
     def initialize
         @rows = Array.new(8) {Array.new(8)}
+        @rows[0].map!{|el| el = Piece.new}
+        @rows[1].map!{|el| el = Piece.new}
+        @rows[6].map!{|el| el = Piece.new}
+        @rows[7].map!{|el| el = Piece.new}   
     end
 
     def [](pos)
@@ -13,9 +18,18 @@ class Board
         @rows[pos[0]][pos[1]] = val
     end
 
+    def move_piece(start_pos, end_pos)
+       raise "Error no" if @rows[start_pos] == nil 
+       if end_pos.all?{|cord| cord >= 0 && cord < 8}
+            piece = @rows[start_pos] 
+            @rows[end_pos] = piece 
+            @rows[start_pos] = nil
+        end 
+    end
 end
 
-test = Board.new
-pos = [0,1]
-p test[pos] = "A"
-p test
+# test = Board.new
+# p test
+# # pos = [0,1]
+# # p test[pos] = "A"
+# # p test
