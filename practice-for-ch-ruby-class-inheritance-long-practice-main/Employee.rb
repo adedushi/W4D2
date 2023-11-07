223b6ce6d9b5f92f4fa17e11a9b288b76328d1fa
Shaun Jhingoor <Jhingoor1945@gmail.com>
2023-11-07 14:06:16 -0500

    Attempt employee

--- practice-for-ch-ruby-class-inheritance-long-practice-main/Employee.rb
+++ practice-for-ch-ruby-class-inheritance-long-practice-main/Employee.rb
@@ -1,10 +1,13 @@
 class Employee
-    
-    def initialize(name, title, salary, boss)
-        @name = @name
+    attr_accessor :boss
+    def initialize(name, title, salary,boss)
+        @name = name
         @title = title
         @salary = salary
         @boss = boss
     end
-    
+    def assign_boss
+        self.boss = new_boss
+        new_boss.employees << self
+    end
 end
--- practice-for-ch-ruby-class-inheritance-long-practice-main/Manager.rb
+++ practice-for-ch-ruby-class-inheritance-long-practice-main/Manager.rb
@@ -1,10 +1,10 @@
 require_relative "Employee"
 
 class Manager < Employee
-    
+    attr_accessor :employees
     def initialize(name, title, salary, boss)
         super
-        emmployees = []
+        employees = []
     end
 
 
