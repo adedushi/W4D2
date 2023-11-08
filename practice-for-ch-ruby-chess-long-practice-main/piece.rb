6e6f2434f46eec767a417000f6f00c4017d5b31b
Shaun Jhingoor <Jhingoor1945@gmail.com>
2023-11-08 11:28:34 -0500

    Complete piece

--- practice-for-ch-ruby-chess-long-practice-main/piece.rb
+++ practice-for-ch-ruby-chess-long-practice-main/piece.rb
@@ -1,2 +1,26 @@
-class Piece 
+class Piece
+    attr_reader :color, :board, :pos
+    def initialize(color,board,pos)
+        @color = color.to_sym
+        @board=board
+        @pos=pos
+    end
+    def to_s
+        color = @color.to_s 
+        color 
+    end
+
+    def empty? 
+        @pos.empty?
+    end
+
+    def valid_moves
+       valid = []
+       valid << @pos if !empty? && @pos.all?{|coord| coord < 8 && coord > = 0}
+       valid 
+    end
+
+    def pos=(val)
+        @pos = val 
+    end
 end
