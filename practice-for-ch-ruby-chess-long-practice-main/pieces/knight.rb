f1a5f34523bf6607cdff17411adef366db39a22e
Albert Dedushi <adedushi@hotmail.com>
2023-11-08 11:53:36 -0500

    Update knight and stepable

--- practice-for-ch-ruby-chess-long-practice-main/pieces/knight.rb
+++ practice-for-ch-ruby-chess-long-practice-main/pieces/knight.rb
@@ -11,6 +11,24 @@ class Knight < Piece
   protected
 
   def move_diffs
-    # return an array of diffs representing where a Knight can step to
+    moves = [[1, 2], 
+    [2, 1],
+    [2, -1],
+    [1, -2],
+    [-1, 2],
+    [-2, 1],
+    [-1, -2], 
+    [-2, -1]]
+    
+    array = []
+    x, y = self.pos 
+
+    moves.each do |move| 
+        dx, dy = move
+        new_move = [dx + x, dy + y]
+        array << new_move if valid_moves.include?(new_move)
+    end
+
+    array
   end
 end
